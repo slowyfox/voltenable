7217af2615025ce849f99792c8c2033973c5a0be
slowyfox <157892222+slowyfox@users.noreply.github.com>
2024-01-28 06:50:51 +0000

    Fix permission

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-adb shell sh -c 'umask 047; rm -r /data/local/tmp/voltenable 2>/dev/null; mkdir /data/local/tmp/voltenable'
+adb shell mkdir -m 750 /data/local/tmp/voltenable
 adb push shelltool.jar /data/local/tmp/voltenable/
 adb push shellscript.sh /data/local/tmp/voltenable/
 adb shell sh /data/local/tmp/voltenable/shellscript.sh "$@"
+adb shell rm -r /data/local/tmp/voltenable
